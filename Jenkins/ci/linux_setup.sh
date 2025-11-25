c35ff7cd5f41ec1bec0500466c8c8e3e4cdc34a8
leeyeonjung <asa4828482@gmail.com>
2025-11-25 17:58:50 +0900

    Add: git pull logic

--- Jenkins/ci/linux_setup.sh
+++ Jenkins/ci/linux_setup.sh
@@ -3,6 +3,12 @@
 echo "[Linux] Moving to project root"
 cd /home/ubuntu/saucedemo
 
+echo "[Linux] Fetching latest Git remote"
+git fetch --all
+
+echo "[Linux] Resetting to origin/main"
+git reset --hard origin/main
+
 echo "[Linux] Activating venv"
 source /home/ubuntu/saucedemo/saucedemo_pytest/bin/activate
 
--- Jenkins/ci/win_setup.bat
+++ Jenkins/ci/win_setup.bat
@@ -1,5 +1,11 @@
-echo [Windows] Changing directory
+echo [Windows] Moving to project root
 cd C:\Automation\saucedemo
+
+echo [Windows] Fetching latest Git remote
+git fetch --all
+
+echo [Windows] Resetting to origin/main
+git reset --hard origin/main
 
 echo [Windows] Installing Playwright browser
 playwright install chromium
